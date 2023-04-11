18e2a9a87e7ccae90fa42ed2a78908864add38e9
Macy Crow <macr9723@colorado.edu>
2023-04-11 12:29:27 -0600

    removed uncessary variables and tables for 4/12 rec

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS users(
     password VARCHAR(50) NOT NULL
     --email VARCHAR(200) NOT NULL,
     --age INTEGER NOT NULL,
-    --user_platform VARCHAR(50) NOT NULL
+    --user_platform VARCHAR(50) NOT NULL,
+    --game library id
 );
 
 -- decimal(p,s) p is the amount of #'s allowed before . s is amount allowed after
@@ -13,30 +14,30 @@ CREATE TABLE IF NOT EXISTS games(
     --avg_rating DECIMAL(2,1) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS game_libraries(
-    lib_id NUMERIC PRIMARY KEY,
-    review VARCHAR(250) NOT NULL,
-    user_rating DECIMAL(2,1) NOT NULL,
-    username VARCHAR(50) NOT NULL REFERENCES users (username),
-    game VARCHAR(100) NOT NULL REFERENCES games (name)
-);
+--CREATE TABLE IF NOT EXISTS game_libraries(
+    --lib_id NUMERIC PRIMARY KEY,
+    --review VARCHAR(250) NOT NULL,
+    --user_rating DECIMAL(2,1) NOT NULL,
+    --username VARCHAR(50) NOT NULL REFERENCES users (username),
+    --game VARCHAR(100) NOT NULL REFERENCES games (name)
+--);
 
-CREATE TABLE IF NOT EXISTS genres (
-    genre_id NUMERIC PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-);
+--CREATE TABLE IF NOT EXISTS genres (
+    --genre_id NUMERIC PRIMARY KEY,
+    --name VARCHAR(50) NOT NULL
+--);
 
-CREATE TABLE IF NOT EXISTS platforms (
-    platform_id NUMERIC PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-);
+--CREATE TABLE IF NOT EXISTS platforms (
+    --platform_id NUMERIC PRIMARY KEY,
+    --name VARCHAR(50) NOT NULL
+--);
 
-CREATE TABLE IF NOT EXISTS platforms_to_games(
-    plaform_id INTEGER NOT NULL REFERENCES platforms (platform_id),
-    game VARCHAR(100) NOT NULL REFERENCES games (name)
-);
+--CREATE TABLE IF NOT EXISTS platforms_to_games(
+    --plaform_id INTEGER NOT NULL REFERENCES platforms (platform_id),
+    --game VARCHAR(100) NOT NULL REFERENCES games (name)
+--);
 
-CREATE TABLE IF NOT EXISTS genres_to_games(
-    genre_id INTEGER NOT NULL REFERENCES genres (genre_id),
-    game VARCHAR(100) NOT NULL REFERENCES games (name)
-);
+--CREATE TABLE IF NOT EXISTS genres_to_games(
+    --genre_id INTEGER NOT NULL REFERENCES genres (genre_id),
+    --game VARCHAR(100) NOT NULL REFERENCES games (name)
+--);
