0f377ba74a15607918b1e69423fde37e60850a17
Macy Crow <macr9723@colorado.edu>
2023-04-11 12:38:30 -0600

    added insert for users, username is your git, password is 123456

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(50) NOT NULL
-    --email VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
     --age INTEGER NOT NULL,
     --user_platform VARCHAR(50) NOT NULL,
     --game library id
