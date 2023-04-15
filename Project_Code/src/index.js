ee280e3971dded56de2c6830bcff83979b9af510
Alakh Naik <alna3985@colorado.edu>
2023-04-14 22:13:57 -0600

    Added Initial FE-BE for Home(Discover) and Search Pages-Displaying Game Images. Also Minor changes to register route, create.sql and insert.sql(commented out). Also Added test case to server.spec.js.

--- Project_Code/src/index.js
+++ Project_Code/src/index.js
@@ -66,46 +66,100 @@ app.use(
 // <!-- Section 4 : API Routes -->
 // *****************************************************
 
-<<<<<<< HEAD
 app.get('/', (req, res) => {
   res.render('pages/login');
 });
 
-
 app.get('/register', (req, res) => {
     res.render('pages/register', {});
-=======
+});
 
-app.get('/register', (req, res) => {
-    res.render('pages/register');
->>>>>>> 45d7ac7c6f27d1eae9bf7dbf9d75bc9495ef2cdb
+// Welcome test case
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
 });
   
 // Register
 app.post('/register', async (req, res) => {
-    //hash the password using bcrypt library
-    const username = req.body.username;
-    const hash = await bcrypt.hash(req.body.password, 10);
-  
-    const query = "insert into users (username,password) values $1, $2";
-    const values = [username, hash];
-    db.one(query,values)
-    .then((data) => {
-      user.username = username;
-      user.password = hash;
-  
-      req.session.user = user;
-      req.session.save();
-  
-      res.redirect("/login");
-    })
-    .catch((err) => {
-      console.log(err);
-      res.redirect("/register");
-    });
-    // To-DO: Insert username and hashed password into 'users' table
-    
+  //hash the password using bcrypt library
+  const username = req.body.username;
+  const hash = await bcrypt.hash(req.body.password, 10);
+
+  const query = "insert into users (username,password) values ($1, $2) returning *;";
+  const values = [username, hash];
+  db.one(query,values)
+  .then((data) => {
+    user.username = username;
+    user.password = hash;
+
+    req.session.user = user;
+    req.session.save();
+
+    res.redirect("/login");
+  })
+  .catch((err) => {
+    console.log(err);
+    res.redirect("/register");
+  });
+  // To-DO: Insert username and hashed password into 'users' table
+
 });
+
+// LOGIN API
+
+// Render Login
+app.get('/login', (req, res) => {
+	res.render("pages/login");
+});
+
+// Try/catch *** do we want to keep db of usernames?? ***
+
+app.post('/login', async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		// Find user by username
+		const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', username);
+
+		if (!user) {
+			// User not found, redirect to register page
+			res.redirect("pages/register");
+			return;
+		}
+
+		// Compare password from request with password in DB
+		const match = await bcrypt.compare(password, user.password);
+
+		if (!match) {
+			// Passwords don't match, throw error
+			throw new Error('Incorrect username or password.');
+		}
+
+		// Passwords match, save user in session
+		req.session.user = user;
+		req.session.save();
+    //res.json({status: 'success', message: 'Success'});
+
+		// Redirect to discover page
+    // NO DISCOVER PAGE, just API for now.
+		res.redirect('/discover');
+	} catch (error) {
+		// Handle error and render login page with error message
+		res.render('pages/login', { errorMessage: error.message });
+	}
+});
+
+// Authentication Middleware.
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    // Default to login page.
+    return res.redirect('/login');
+  }
+  next();
+};
+
+// Authentication Required
+app.use(auth);
 
 app.get('/discover', (req, res) => {
   const latestGamesRequest = axios({
@@ -162,8 +216,9 @@ app.get('/discover', (req, res) => {
 
 app.get('/search', (req,res)=>{
 
+  const search = req.query.search;
   axios({
-  url: `https://www.giantbomb.com/api/search`,
+  url: "https://www.giantbomb.com/api/search",
   method: 'GET',
   dataType: 'json',
   headers: {
@@ -173,24 +228,24 @@ app.get('/search', (req,res)=>{
     api_key: process.env.API_KEY,
     limit: 10,
     format: 'json',
-    query: 'Call of Duty', // this will be replaced with req.body.search, allowing the user to pass in a game title to search for, replace this for now with whatever dummy game name for testing
+    query: search, // this will be replaced with req.body.search, allowing the user to pass in a game title to search for, replace this for now with whatever dummy game name for testing
     resources:'game',
   },
-})
+  })
   .then(results => {
     console.log(results.data.results); // the results will be displayed on the terminal if the docker containers are running // Send some parameters
-    res.render('pages/discover', {
+    res.render('pages/search', {
       results,
     });
   })
   .catch(error => {
     console.log(error);
-    res.render("pages/discover", {
+    res.render("pages/search", {
       results: [],
     });
     // Handle errors
   });
-});
+  });
 
 
 app.get('/logout', (req, res) => {
@@ -199,55 +254,6 @@ app.get('/logout', (req, res) => {
 });
 
 
-
-// LOGIN API
-
-// Render Login
-app.get('/login', (req, res) => {
-	res.render("pages/login");
-});
-
-// Try/catch *** do we want to keep db of usernames?? ***
-
-app.post('/login', async (req, res) => {
-	const { username, password } = req.body;
-
-	try {
-		// Find user by username
-		const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', username);
-
-		if (!user) {
-			// User not found, redirect to register page
-			res.redirect("pages/register");
-			return;
-		}
-
-		// Compare password from request with password in DB
-		const match = await bcrypt.compare(password, user.password);
-
-		if (!match) {
-			// Passwords don't match, throw error
-			throw new Error('Incorrect username or password.');
-		}
-
-		// Passwords match, save user in session
-		req.session.user = user;
-		req.session.save();
-
-		// Redirect to discover page
-    // NO DISCOVER PAGE, just API for now.
-		// res.redirect('/discover');
-	} catch (error) {
-		// Handle error and render login page with error message
-		res.render('pages/login', { errorMessage: error.message });
-	}
-});
-
-// lab11 Adding sample route to index.js
-
-app.get('/welcome', (req, res) => {
-  res.json({status: 'success', message: 'Welcome!'});
-});
 
 // *****************************************************
 // <!-- Section 5 : Start Server-->
