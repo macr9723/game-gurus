fed9c0589899d3e5848f529be53de7a6b53420c6
Jared Dutton <lgtrib7504@gmail.com>
2023-04-26 11:52:03 -0600

    quickfix for checking if game already exists

--- Project_Code/src/index.js
+++ Project_Code/src/index.js
@@ -359,36 +359,29 @@ app.post('/add_game', async (req,res) => {
   try {
     console.log("game_id:", game_id);
     const [gameFound] = await db.any(`select game_id from games where game_id = $1;`, [game_id]);
+    if (!gameFound) {
       db.any(insertGame, [game_id, name])
-        .then(function (data) {
-          // res.status(201).json({
-          // status: 'success',
-          // data: data,
-          // });
-          res.redirect('/discover');
-        })
+        .then(
+          res.redirect('/discover')
+        )
         .catch(function (err) {
           return console.log(err);
         });
-
+    }
   
     // If the game is already in our database we can update the rest of the tables accordingly
     const [newReview] = await db.any(insertReview, [review, rating])
     const [newEntry] = await db.any(insertEntry, [game_id, newReview.review_id])
     db.any(insertUsers_to_Entries, [req.session.user.username, newEntry.entry_id])
-      .then(function (data) {
-        res.status(201).json({
-          status: 'success',
-          data: data,
-        });
-      })
+      .then(
+        res.redirect('discover')
+      )
       .catch(function (err) {
         return console.log(err);
       })
   } catch (error) {
     // res.render('/discover', { message: error.message });
     res.redirect('/discover');
-
   }
   } else {
     res.render('pages/login', { message: 'Please login to add a game' });
